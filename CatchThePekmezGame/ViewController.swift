302e64b7bedb64acbae2b009e3456dd50c2379d1
cardaozel <arda.civan@gmail.col>
2024-11-24 02:03:17 +0300

    add the targets

--- CatchThePekmezGame/ViewController.swift
+++ CatchThePekmezGame/ViewController.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var highScoreLabel: UILabel!
+    @IBOutlet weak var pekmez1: UIImageView!
+    @IBOutlet weak var pekmez2: UIImageView!
+    @IBOutlet weak var pekmez3: UIImageView!
+    @IBOutlet weak var pekmez4: UIImageView!
+    @IBOutlet weak var pekmez5: UIImageView!
+    @IBOutlet weak var pekmez6: UIImageView!
+    @IBOutlet weak var pekmez7: UIImageView!
+    @IBOutlet weak var pekmez8: UIImageView!
+    @IBOutlet weak var pekmez9: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
